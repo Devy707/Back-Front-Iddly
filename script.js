05ed6ac6d8b4dfd029004c0df4178de30ceadc91
Bogdan <stetsenko.bogdan.work@gmail.com>
2021-10-03 20:45:09 +0300

    end all

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-   const slider = new ChiefSlider('.slider', {
-     loop: false
-   });
- });
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  event.preventDefault();
+  const slider = new ChiefSlider('.slider', {
+    loop: true,
+    autoplay: true,
+    interval: 7000,
+  });
+});
